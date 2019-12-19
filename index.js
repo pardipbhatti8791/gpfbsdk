16b124b1f11aaef8adb08d811c90a8712118379e
Pardip Bhatti <gpcoders.pardip@gmail.com>
2019-12-19 11:54:36 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,26 @@
+function loadFbSDK(options) {
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId: "366484310574194",
+      autoLogAppEvents: true,
+      xfbml: true,
+      version: "v2.11"
+    });
+
+    // Broadcast an event when FB object is ready
+    var fbInitEvent = new Event("FBObjectReady");
+    document.dispatchEvent(fbInitEvent);
+  };
+
+  (function(d, s, id) {
+    var js,
+      fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {
+      return;
+    }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  })(document, "script", "facebook-jssdk");
+}
